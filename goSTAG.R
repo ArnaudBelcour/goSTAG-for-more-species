deebdc784149dff4ad37cfbbb474f1b61650b355
Arnaud Belcour <arnaud.belcour@irisa.fr>
2018-05-02 15:11:01 +0200

    Add test data for GMT manipulation.

--- goSTAG.R
+++ goSTAG.R
@@ -1,16 +1,27 @@
-"""
+#!/usr/bin Rscript
+
+"
 This script makes a goSTAG analysis using the go_gene.gmt and the DE_gene.gmt files.
 go_gene.gmt contains the GO terms and all the genes they annotate.
 DE_gene.gmt contains the DE gene lists and all the genes they contain.
 These files are create by gmt_DE_gene_creation.py and gmt_go_genome_creation.py
-"""
+"
+
+args <- commandArgs(TRUE)
+gene_gmt <- args[1]
+go_gmt <- args[2]
+
+# Test data.
+go_terms_check <- loadGOTerms()
+data( goSTAG_example_gene_lists )
+genes_check <- head( lapply( goSTAG_example_gene_lists, head ) )
 
 # goSTAG analysis
 source("https://bioconductor.org/biocLite.R")
 biocLite("goSTAG")
 library(goSTAG)
-gos <- loadGeneLists( "go_gene.gmt" )
-genes <- loadGeneLists( "DE_gene.gmt" )
+gos <- loadGeneLists( "test/test_data/example_go_terms.gmt" )
+genes <- loadGeneLists("test/test_data/example_DE_gene.gmt" )
 gos[["ALL"]] <- unique(rapply(gos, function(x) head(x,Inf)))
 enrichment_matrix <- performGOEnrichment( genes, gos, p.adjust_method = 'BH')
 hclust_results <- performHierarchicalClustering( enrichment_matrix )
